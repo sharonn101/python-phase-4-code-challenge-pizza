c5abb4a6e60c7f7cbb107418a69752993d6814f7
Sharon <sharonirungu2006@gmail.com>
2025-09-22 23:31:06 +0300

    done

--- server/app.py
+++ server/app.py
@@ -24,6 +24,46 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route("/restaurants", methods=["GET"])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return [restaurant.to_dict(rules=['-restaurant_pizzas']) for restaurant in restaurants], 200
+
+@app.route("/restaurants/<int:id>", methods=['GET', 'DELETE'])
+def get_restaurant(id):
+    restaurant_by_id = Restaurant.query.filter(Restaurant.id == id).first()
+    if not restaurant_by_id:
+        return {"error": "Restaurant not found"}, 404
+
+    if request.method == 'GET':
+        return restaurant_by_id.to_dict(), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(restaurant_by_id)
+        db.session.commit()
+        return {}, 204
+
+@app.route("/pizzas", methods=["GET"])
+def get_pizzas():
+    return [pizza.to_dict(rules=['-restaurant_pizzas']) for pizza in Pizza.query.all()], 200
+
+@app.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    try:
+        new_pizza = RestaurantPizza(
+            price=data.get("price"),
+            pizza_id=data.get("pizza_id"),
+            restaurant_id=data.get("restaurant_id"),
+        )
+    except ValueError as e:
+        return {"errors": ["validation errors"]}, 400
+
+    db.session.add(new_pizza)
+    db.session.commit()
+
+    return new_pizza.to_dict(), 201
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,8 +21,9 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant')
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +37,9 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +50,23 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
     # add relationships
-
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
     # add serialization rules
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas',)
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if not (1 <= price <= 30):
+            raise ValueError("Price must be beteen 1 and 30.")
+        else:
+            return price
+
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
